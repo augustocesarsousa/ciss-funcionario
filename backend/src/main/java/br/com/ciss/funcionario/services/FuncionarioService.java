3789ff30bb9a42054618cc99dc2ef6c1348f435f
Augusto Cesar <acs-03@hotmail.com>
2023-04-03 01:16:21 -0300

    FuncionarioService adicionado metodo findByFilterPaged

--- backend/src/main/java/br/com/ciss/funcionario/services/FuncionarioService.java
+++ backend/src/main/java/br/com/ciss/funcionario/services/FuncionarioService.java
@@ -3,8 +3,11 @@ package br.com.ciss.funcionario.services;
 import br.com.ciss.funcionario.dtos.FuncionarioDTO;
 import br.com.ciss.funcionario.entities.Funcionario;
 import br.com.ciss.funcionario.repositories.FuncionarioRepository;
+import br.com.ciss.funcionario.repositories.FuncionarioCustomRepository;
 import br.com.ciss.funcionario.utils.CopyDtoToEntity;
 import br.com.ciss.funcionario.services.exceptions.ResourceNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,11 @@ import java.util.Optional;
 public class FuncionarioService {
 
     private FuncionarioRepository funcionarioRepository;
+    private FuncionarioCustomRepository funcionarioCustomRepository;
 
-    public FuncionarioService(FuncionarioRepository funcionarioRepository) {
+    public FuncionarioService(FuncionarioRepository funcionarioRepository, FuncionarioCustomRepository funcionarioCustomRepository) {
         this.funcionarioRepository = funcionarioRepository;
+        this.funcionarioCustomRepository = funcionarioCustomRepository;
     }
 
     @Transactional
@@ -39,4 +44,11 @@ public class FuncionarioService {
         return new FuncionarioDTO(funcionario);
     }
 
+    @Transactional(readOnly = true)
+    public Page<FuncionarioDTO> findByFilterPaged(String idString, String nome, String sobrenome, String email, String nis, Pageable pageable) {
+        Long id = null;
+        if(idString != null) id = Long.parseLong(idString);
+        Page<Funcionario> page = funcionarioCustomRepository.findByFilter(id, nome, sobrenome, email, nis, pageable);
+        return page.map(funcionario -> new FuncionarioDTO(funcionario));
+    }
 }
