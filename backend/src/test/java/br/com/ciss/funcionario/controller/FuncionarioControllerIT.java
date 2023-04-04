8a2425b653fbba9e2cbb919b1d51072b7c85f029
Augusto Cesar <acs-03@hotmail.com>
2023-04-04 03:08:45 -0300

    Teste de integracao FuncionarioControllerIT createShouldReturnUnprocessableEntityWhenNisIsInvalid

--- backend/src/test/java/br/com/ciss/funcionario/controller/FuncionarioControllerIT.java
+++ backend/src/test/java/br/com/ciss/funcionario/controller/FuncionarioControllerIT.java
@@ -82,6 +82,19 @@ public class FuncionarioControllerIT {
     }
 
     @Test
+    public void createShouldReturnUnprocessableEntityWhenNisIsInvalid() throws Exception {
+        funcionarioDTO.setNis("a");
+        String jsonBody = objectMapper.writeValueAsString((funcionarioDTO));
+
+        mockMvc.perform(post("/funcionarios")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.errors[0].message").value("O NIS precisa ter 11 dígitos numéricos!"));
+    }
+
+    @Test
     public void findByFilterPagedShouldReturnSortedPageWhenSortByName() throws Exception{
         mockMvc.perform(get("/funcionarios?page=0&size=12&sort=nome,asc"))
                 .andExpect(status().isOk())
