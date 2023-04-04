56a5bde9b8fb8f6b4b0c82391e2cf3db232e651d
Augusto Cesar <acs-03@hotmail.com>
2023-04-04 01:42:35 -0300

    Teste de integracao FuncionarioControllerIT findByFilterPagedShouldReturnSortedPageWhenSortByName

--- backend/src/test/java/br/com/ciss/funcionario/controller/FuncionarioControllerIT.java
+++ backend/src/test/java/br/com/ciss/funcionario/controller/FuncionarioControllerIT.java
@@ -30,10 +30,12 @@ public class FuncionarioControllerIT {
     private ObjectMapper objectMapper;
 
     private FuncionarioDTO funcionarioDTO;
+    private long totalFuncionarios;
 
     @BeforeEach
     void setUp() throws Exception {
         funcionarioDTO = Factory.createFuncionarioDtoTest();
+        totalFuncionarios = 15L;
     }
 
     @Test
@@ -49,6 +51,16 @@ public class FuncionarioControllerIT {
                 .andExpect(jsonPath("$.nome").exists());
     }
 
+    @Test
+    public void findByFilterPagedShouldReturnSortedPageWhenSortByName() throws Exception{
+        mockMvc.perform(get("/funcionarios?page=0&size=12&sort=nome,asc"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalElements").value(totalFuncionarios))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.content[0].nome").value("Anchard"))
+                .andExpect(jsonPath("$.content[1].nome").value("Anne"))
+                .andExpect(jsonPath("$.content[2].nome").value("Betrich"));
 
+    }
 
 }
