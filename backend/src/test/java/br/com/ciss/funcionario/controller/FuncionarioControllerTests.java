3711cad677272e83304bf71e34ef4e47159cff97
Augusto Cesar <acs-03@hotmail.com>
2023-04-03 21:32:12 -0300

    Teste unitario FuncionarioController createShouldReturnCreated

---
+++ backend/src/test/java/br/com/ciss/funcionario/controller/FuncionarioControllerTests.java
@@ -0,0 +1,54 @@
+package br.com.ciss.funcionario.controller;
+
+import br.com.ciss.funcionario.dtos.FuncionarioDTO;
+import br.com.ciss.funcionario.services.FuncionarioService;
+import br.com.ciss.funcionario.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FuncionarioController.class)
+public class FuncionarioControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private FuncionarioService funcionarioService;
+
+    private FuncionarioDTO funcionarioDTO;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        funcionarioDTO = Factory.createFuncionarioDtoTest();
+
+        when(funcionarioService.create(any())).thenReturn(funcionarioDTO);
+    }
+
+    @Test
+    public void createShouldReturnCreated() throws Exception {
+        String jsonBody = objectMapper.writeValueAsString((funcionarioDTO));
+
+        mockMvc.perform(post("/funcionarios")
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.nome").exists());
+    }
+}
